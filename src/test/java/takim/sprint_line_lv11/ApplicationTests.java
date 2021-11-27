0bbb1e5514e3d843a07008aeb945748861c1bfe1
xogml123 <xogml951@gmail.com>
2021-11-28 01:32:02 +0900

    Complete Java Spring project configure

---
+++ src/test/java/takim/sprint_line_lv11/ApplicationTests.java
@@ -0,0 +1,13 @@
+package takim.sprint_line_lv11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
